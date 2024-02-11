d2b9e844a2e3f05a43037dd9eddc6635df604517
Burak <Burak@DESKTOP-6CG2926>
2024-02-11 20:26:38 +0300

    Add project files.

---
+++ intro/Business/CourseManager.cs
@@ -0,0 +1,41 @@
+﻿using intro.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace intro.Business;
+
+public class CourseManager
+{
+    Course[] courses = new Course[3];
+    public CourseManager()
+    {
+        Course course1 = new();
+        course1.Id = 1;
+        course1.Name = "C# Kursu";
+        course1.Description = ".Net 8 vs...";
+        course1.Price = 0;
+
+        Course course2 = new();
+        course2.Id = 1;
+        course2.Name = "JAVA Kursu";
+        course2.Description = "JAVA 17 vs...";
+        course2.Price = 10;
+
+        Course course3 = new();
+        course3.Id = 1;
+        course3.Name = "Python Kursu";
+        course3.Description = "Python 3.12 vs...";
+        course3.Price = 20;
+
+        courses[0] = course1;
+        courses[1] = course2;
+        courses[2] = course3;
+    }
+    public Course[] GetAll()
+    {
+        return courses;
+    }
+}
---
+++ intro/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+//double total = 0;
+//while (true)
+//{
+
+//    double last = 0;
+//    Console.Clear();
+//    Console.ForegroundColor = ConsoleColor.Green;
+//    Console.WriteLine("Toplam:" + total);
+//    Console.ForegroundColor = ConsoleColor.White;
+//    Console.Write("Toplanacak sayıyı giriniz: ");
+//    bool isDouble = double.TryParse(Console.ReadLine(), out last);
+//    if (isDouble)
+//    {
+//        total += last;
+//    }
+//    else
+//    {
+//        Console.ForegroundColor = ConsoleColor.Red;
+//        Console.WriteLine("Lütfen sadece sayı giriniz.");
+//        Console.ReadKey();
+//    }
+//}
+
+//string[] loans = { "kredi1", "kredi2", "kredi3", "kredi4", "kredi5", "kredi6" };
+////string[] loans2 = new string[6];
+////loans2[2] = "test";
+
+//for (int i = 0; i < loans.Length; i++)
+//{
+//    Console.WriteLine(loans[i]);
+//}
+
+using intro.Business;
+using intro.Entities;
+
+CourseManager courseManager = new();
+Course[] courses = courseManager.GetAll();
+for (int i = 0; i < courses.Length; i++)
+{
+    Console.WriteLine(courses[i].Name + " / " + courses[i].Price);
+}
